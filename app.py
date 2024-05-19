bd2197ef115af588886782196c43d275f48f7eba
hdcardon <hdcardon@gmail.com>
2024-05-19 18:35:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ with st.sidebar:
     
     selected_year = st.selectbox('Select a year', year_list, index=len(year_list)-1)
     df_selected_year = df_reshaped[df_reshaped.year == selected_year]
-    df_selected_year_sorted = df_selected_year.sort_values(by="population", ascending=False)
+    #df_selected_year_sorted = df_selected_year.sort_values(by="population", ascending=False)
 
     color_theme_list = ['blues', 'cividis', 'greens', 'inferno', 'magma', 'plasma', 'reds', 'rainbow', 'turbo', 'viridis']
     selected_color_theme = st.selectbox('Select a color theme', color_theme_list)
